4e8e4b9757fee8ec619c173b901ea738f4cf45be
Daniel Łobacz <229363@student.pwr.edu.pl>
2020-04-28 21:05:47 +0200

    heap bug

--- backend/index.js
+++ backend/index.js
@@ -11,25 +11,73 @@ const pool = new Pool({
   port: 5432,
 })
 
-let formGroups = (list, size) => {
-  let numOfGroups = Math.ceil(list.length / size);
-  const groups = new Array(numOfGroups).fill([]);
 
-  for(let group of groups) {
-    group.push(list.pop());
+let isTheSameDistrict = (group, employee) => {
+  console.log('district')
+  for(let other of group)
+  {
+    if(other.district === employee.district) return true;
+  }
+  return false;
+}
+
+function isTheSameDepartment (group, employee) {
+  console.log('department')
+  for(let other of group)
+  {
+    if(other.department === employee.department) return true;
+  }
+  return false;
+}
+
+function isTheSameAge (group, employee) {
+  console.log('age')
+  for(let other of group)
+  {
+    if(other.age === employee.age) return true;
+  }
+  return false;
+}
+
+function isGroupAvailable (group, size, employee) {
+  let chance = Math.random() < 0.2;
+  let ageChance = Math.random() < 0.5;
+  console.log(group.length, group.length < size);
+  if(chance) { 
+    return group.length < size; 
+  } else { 
+    return group.length < size && 
+              !isTheSameDepartment(group, employee) && 
+              !isTheSameDistrict(group, employee) && 
+              (!isTheSameAge(group, employee) || ageChance);
+  }
+}
+
+
+let formGroups = (list, size) => {
+  const numOfGroups = Math.ceil(list.length / size);
+  let groups = new Array(numOfGroups).fill(new Array());
+  for(let i = 0; i < numOfGroups; ++i)
+  {
+    groups[i].push(list.pop());
+  }
+  while(list.length > 0) {
+    for(let i=0; i < numOfGroups; ++i){
+      console.log(i)
+      if(isGroupAvailable(groups[i], size, list[0])) 
+      {
+        groups[i].push(list.shift());
+      }
+      if(list.length == 0) {
+        break;
+      }
+    }
+    if(list.length == 0) {
+      break;
+    }
   }
 
-  // while(list.length > 0) {
-  //   for(let group of groups) {
-  //     if(group.length < 5)
-  //     {
-        
-  //     }
-  //     if(list.length == 0) break;
-  //   }
-  //   if(list.length == 0) break;
-  // }
-
+  console.log('ended')
   return groups;
 }
 
@@ -41,6 +89,7 @@ app.get('/employees/:size', (request, response) => {
       response.status(400).json({ info: `Error` });
       throw error;
     }
+    
     let groups = formGroups(results.rows, request.params.size);
     response.status(200).json(groups)
   })
